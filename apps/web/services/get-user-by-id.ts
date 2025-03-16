48f91e6c41ae07eb14c2443e1ef91e8508049b75
MrRobot-fm <federicomigliore87@gmail.com>
2025-03-16 16:07:49 +0100

    chore(github-action): setup CI with github

--- apps/web/services/get-user-by-id.ts
+++ apps/web/services/get-user-by-id.ts
@@ -1,5 +1,5 @@
 export const getUserById = async ({ id }: { id: string }) => {
-  const baseUrl = process.env.API_BASE_URL;
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
   if (!baseUrl) {
     throw new Error("Missing API_BASE_URL env variable");
