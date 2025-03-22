2bc68d6dd58c0be80bdac072c2a8da33d02fdabe
MrRobot-fm <federicomigliore87@gmail.com>
2025-03-22 17:57:00 +0100

    chore(cypress-config): added cypress to web workspace and refactored github actions

---
+++ apps/web/cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      const configOverrides: Partial<Cypress.PluginConfigOptions> = {
+        baseUrl: "http://localhost:3000",
+        viewportWidth: 1440,
+        viewportHeight: 1024,
+      };
+      on("task", {
+        log: (message) => {
+          console.log(message);
+
+          return null;
+        },
+      });
+
+      return { ...config, ...configOverrides };
+    },
+  },
+});
