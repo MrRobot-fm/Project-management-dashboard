d5e7092974820d0d10c4370f34c3cea4a579b403
MrRobot-fm <federicomigliore87@gmail.com>
2025-03-22 20:47:58 +0100

    chore(cypress-config): refactored cypress config

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -1,22 +1,9 @@
 import { defineConfig } from "cypress";
 
-export const config = defineConfig({
+const config = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      const configOverrides: Partial<Cypress.PluginConfigOptions> = {
-        baseUrl: "http://localhost:3000",
-        viewportWidth: 1440,
-        viewportHeight: 1024,
-      };
-
-      on("task", {
-        log: (message) => {
-          console.log(message);
-          return null;
-        },
-      });
-
-      return { ...config, ...configOverrides };
-    },
+    setupNodeEvents(on, config) {},
   },
 });
+
+export default config;
