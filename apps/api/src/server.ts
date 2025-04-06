b89e728c931508d2a0331055ef59cddcaa2102e6
MrRobot-fm <federicomigliore87@gmail.com>
2025-04-06 13:09:02 +0200

    feat(messages): basic implementation of web socket chat

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,11 +2,25 @@ import express from "express";
 import { errorMiddleware } from "@/middlewares/errors";
 import { rootRouter } from "@/routes/root";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 export const app = express();
 const PORT = process.env.PORT;
 
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
 app.use(cookieParser());
+
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +28,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", rootRouter);
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log(`User connected with id: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+    console.log(data);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- apps/web/app/messages/page.tsx
+++ apps/web/app/messages/page.tsx
@@ -1,5 +1,68 @@
-import React from "react";
+"use client";
+
+import { type FormEvent, useEffect, useState } from "react";
+import { Button } from "@workspace/ui/components/Button";
+import { cn } from "@workspace/ui/lib/utils";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:8001");
+
+type ChatMessage = {
+  text: string;
+  from: "me" | "other";
+};
 
 export default function Messages() {
-  return <div className="p-6">Messages</div>;
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
+
+  const sendMessage = (e: FormEvent) => {
+    if (!message.trim()) return;
+    e.preventDefault();
+    socket.emit("send_message", { message });
+    setMessages((prev) => [...prev, { text: message, from: "me" }]);
+    setMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessages((prev) => [...prev, { text: data.message, from: "other" }]);
+    });
+
+    return () => {
+      socket.off("receive_message");
+    };
+  }, []);
+
+  return (
+    <div className="h-full flex flex-col p-4 relative">
+      <div className="flex-1 overflow-y-auto flex flex-col space-y-2 px-2">
+        {messages.map((msg, index) => (
+          <div
+            key={index}
+            className={cn(
+              "max-w-[75%] px-4 py-1.5 rounded-3xl text-sm break-words font-medium bg-blue-400 self-start text-white",
+              msg.from === "me" && "bg-primary/90 self-end text-white",
+            )}
+          >
+            {msg.text}
+          </div>
+        ))}
+      </div>
+      <form
+        onSubmit={sendMessage}
+        className="pt-4 flex gap-2 items-center absolute bottom-4 left-6 right-6"
+      >
+        <input
+          placeholder="Write a message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="flex-1 py-2 px-5 rounded-full border border-gray-300 focus:outline-none bg-white text-sm"
+        />
+        <Button type="submit" className="opacity-90" disabled={!message}>
+          Send
+        </Button>
+      </form>
+    </div>
+  );
 }
