eee905d3640946eb9a8ee7d3232fbd6cb779c3dc
MrRobot-fm <federicomigliore87@gmail.com>
2025-04-24 16:15:20 +0200

    test(api-workspaces): added timeout to retrieve list of workspaces

--- apps/api/src/tests/integration/workspaces.test.ts
+++ apps/api/src/tests/integration/workspaces.test.ts
@@ -41,6 +41,7 @@ describe("API Workspaces", () => {
     };
 
     await createWorkspace(cookie, newWorkspace.name);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     const response = await getWorkspaces(cookie);
 
