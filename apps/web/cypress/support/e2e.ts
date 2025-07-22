a25bf199cbbe268ce27b0accf6eff367b7e7c4a7
MrRobot-fm <federicomigliore87@gmail.com>
2025-07-22 17:20:48 +0200

    migration(playwright): removed cypress and moved playwright config into packages

--- apps/web/cypress/support/e2e.ts
+++
@@ -1,8 +0,0 @@
-import "./commands";
-import { configure } from "@testing-library/cypress";
-
-configure({ testIdAttribute: "data-test-id" });
-
-Cypress.on("uncaught:exception", (err) => {
-  if (err.message.includes("NEXT_REDIRECT")) return false;
-});
--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,34 +1,14 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
+import playwrightBaseConfig from "@workspace/playwright/config";
 
 export default defineConfig({
+  ...playwrightBaseConfig,
   globalSetup: "./playwright/global-setup.ts",
-  testDir: "./playwright/e2e",
-  fullyParallel: true,
-  forbidOnly: true,
-  retries: 2,
-  workers: 1,
-  reporter: "html",
   use: {
     baseURL: "http://localhost:3000",
     testIdAttribute: "data-test-id",
-
     trace: "on-first-retry",
   },
-
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
+  retries: 2,
+  workers: 1,
 });
